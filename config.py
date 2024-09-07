54e2fbff0584785f984bff34dd288f0004416bb8
Emirhan <76557563+e3ayd@users.noreply.github.com>
2024-09-07 17:01:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# OpenWeatherMap API key
+OPENWEATHERMAP_API_KEY = 'your_openweathermap_api_key_here'
+
+# Weatherbit API key
+WEATHERBIT_API_KEY = 'your_weatherbit_api_key_here'
+
+# Default city for weather queries
+DEFAULT_CITY = 'London'
